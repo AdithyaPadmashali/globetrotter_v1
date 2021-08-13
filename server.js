b0f6e84568d578f59c09dd0c44874e93236ce2d7
abhishek mishra <abhishekmis40@gmail.com>
2021-08-13 13:12:19 +0530

    mental

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
 const express = require("express");
 const { body } = require("express-validator");
 const connectDB = require("./config/db");
-const cors = require("cors");
 const path = require("path");
 const app = express();
 const socketio = require('socket.io')
 
-const { addUser, removeUser, getUser, getUsersInRoom } = require('./helperFunctions/chatHelper.js')
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./helperFunctions/chatHelper.js');
+const { Console } = require("console");
 
-app.use(cors());
 const PORT = process.env.PORT || 5000;
-
+console.log(process.env.NODE_ENV)
 if (process.env.NODE_ENV === 'production') {
-
+    console.log("get working")
     app.use(express.static('client/build'));
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     })
 }
-
+// app.listen(PORT, () => {
+//     console.log("backend started");
+// });
 const server = app.listen(PORT, () => console.log("server started on port " + PORT));
 const io = socketio(server, {
     cors: {
@@ -66,6 +67,3 @@ io.on('connection', (socket) => {
         }
     })
 })
-
-
-
